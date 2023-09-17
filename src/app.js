f9df2df94d37041c5477b3ec77163be511b23210
Luka <lussskki@gmail.com>
2023-09-17 16:18:12 +0400

    feat,post: provide correct token with json object

--- src/app.js
+++ src/app.js
@@ -6,31 +6,29 @@ const app = express()
 import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
 import auth from './middleware/auth.js'
+import users from './users.js'
 
-//development
+//development 
 app.use(bodyParser.json())
-import dotenv from 'dotenv'
+import dotenv from 'dotenv' 
       dotenv.config()
 const TOKEN_PASS = process.env.TOKEN_PASS      
 
 
-const users = [
-    {id: 1, username: 'user1', password: 'password1' },
-    {id: 2, username: 'user2', password: 'password2'}
-]
+
 //Post: login method
-app.post('/login', (req, res) => {
+app.post('/login', auth, (req, res) => {
     const { username, password } = req.body
     const user = users.find(u => u.username === username && u.password === password)
-  
+    
     if (user) {
-      const token = jwt.sign({ userId: user.id }, TOKEN_PASS)
-      res.json({ token })
+        const token = jwt.sign({ username }, TOKEN_PASS, { expiresIn: '1h' })
+        res.json({ token })
     } else {
-      res.status(401).json({ message: 'Authentication failed' })
+        res.status(401).json({ message: 'Authentication failed' })
     }
-  })
-  
+})
+
 
 
 app.listen(3000,()=>{
